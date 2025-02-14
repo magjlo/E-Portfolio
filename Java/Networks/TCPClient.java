065095a7d824241b1bd2a5547668b080c53e4193
magjlo <158214370+magjlo@users.noreply.github.com>
2025-02-14 18:09:39 +0100

    Add files via upload

---
+++ Java/Networks/TCPClient.java
@@ -0,0 +1,55 @@
+
+//Example implementation of a TCP Client that can send data to a server and read incoming data.
+public class TCPClient {
+    
+    public TCPClient() {
+    }
+
+    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
+        
+    
+            
+            Socket sendSocket = new Socket(hostname, port);
+            byte[] recvbuffer = new byte[1024];
+            ByteArrayOutputStream dynarray = new ByteArrayOutputStream();
+
+            OutputStream OS = sendSocket.getOutputStream();
+            OS.write(toServerBytes);
+
+            InputStream IS = sendSocket.getInputStream();
+            int i;
+            int j = 0;
+            while((i = IS.read(recvbuffer)) != -1 && j != 1000000){
+                dynarray.write(recvbuffer, 0, i);
+            }
+        
+            byte[] returnarray = dynarray.toByteArray();
+            sendSocket.close();
+            return returnarray;
+
+           
+    }
+
+    public byte[] askServer(String hostname, int port) throws IOException {
+        
+        
+        
+            Socket recvSocket = new Socket(hostname, port);
+            byte[] recvbuffer = new byte[1024];
+            ByteArrayOutputStream dynarray = new ByteArrayOutputStream();
+        
+            InputStream IS = recvSocket.getInputStream();
+            int i;
+            int j = 0;
+            while((i = IS.read(recvbuffer)) != -1 && j != 1000000){
+                dynarray.write(recvbuffer, 0, i);
+                j++;
+            }
+
+            byte[] returnarray = dynarray.toByteArray();
+            recvSocket.close();
+            return returnarray;
+
+            
+    }
+}
